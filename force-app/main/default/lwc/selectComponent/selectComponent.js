5dab0575bce4b7d65428c5a815eb9978a6af0186
Aneesh B <aneudupi@gmail.com>
2019-08-27 14:10:10 +0530

    Multi-select picklist component

--- force-app/main/default/lwc/selectComponent/selectComponent.js
+++ force-app/main/default/lwc/selectComponent/selectComponent.js
@@ -5,14 +5,18 @@ import getFieldLabel from '@salesforce/apex/PicklistController.getFieldLabel';
 
 export default class SelectComponent extends LightningElement {
     @track options;
-    @track selectedOption;
+    @track selectedOptions = 'Select';
     @track isAttributeRequired = false;
     @api fieldName;
     @api objectName;
     @api controllingFieldName;
+    @api isMultiSelect;
+    @api dropDownLength;
+    @api label;
     contrFieldValue;
     @track fieldLabelName;
     dependentOptions;
+    @track isMouseOver;
 
     connectedCallback() {
         if(this.controllingFieldName) {
@@ -20,7 +24,7 @@ export default class SelectComponent extends LightningElement {
             .then(data => {
                 this.dependentOptions = data;
                 if(this.contrFieldValue) {
-                    this.options = this.dependentOptions[this.contrFieldValue];
+                    this.options = this.getPicklistOptions(this.dependentOptions[this.contrFieldValue]);
                 }
             })
             .catch(error => {
@@ -30,20 +34,22 @@ export default class SelectComponent extends LightningElement {
         else {
             getPickListValues({ objApiName: this.objectName, fieldName: this.fieldName })
             .then(data => {
-                this.options = data;
+                this.options = this.getPicklistOptions(data);
             })
             .catch(error => {
                 this.displayError(error);
             });
         }
 
-        getFieldLabel({objName:this.objectName,fieldName:this.fieldName})
-        .then(data => {
-            this.fieldLabelName = data;
-        })
-        .catch(error => {
-            this.displayError(error);
-        });
+        if(!this.label) {
+            getFieldLabel({objName:this.objectName,fieldName:this.fieldName})
+            .then(data => {
+                this.fieldLabelName = data;
+            })
+            .catch(error => {
+                this.displayError(error);
+            });
+        }
     }
 
     selectionChangeHandler(event) {
@@ -62,6 +68,7 @@ export default class SelectComponent extends LightningElement {
 
     @api
     get controllingFieldValue() {
+        debugger;
         return this.contrFieldValue;
     }
 
@@ -75,5 +82,53 @@ export default class SelectComponent extends LightningElement {
     get isPicklistDisabled() {
         return (this.options &&
                 this.contrFieldValue !== 'Select') ? false: true;
+    }
+
+    get multiSelectClassAttr() {
+        return (this.isMultiSelect) ? 'slds-dropdown__list slds-dropdown--length-'+this.dropDownLength :'';
+    }
+
+    get mainDivClassAttr() {
+        return (this.isMouseOver) ? 'slds-picklist slds-dropdown-trigger slds-dropdown-trigger--click slds-is-open' :
+                                    'slds-picklist slds-dropdown-trigger slds-dropdown-trigger--click';
+    }
+
+    handleMouseEnter() {
+        this.isMouseOver = true;
+    }
+
+    handleMouseLeave() {
+        this.isMouseOver = false;
+    }
+
+    handleSelection(event) {
+        let tickedOptions = '';
+        const selectedLabel = event.target.label;
+        const selectedValue = event.target.checked;
+        if(this.options) {
+            this.options.forEach(function(option){
+                if(option.label === selectedLabel) {
+                    option.isSelected = selectedValue;
+                }
+            });
+            this.options.forEach(function(option){
+                if(option.isSelected) {
+                    tickedOptions += (option.label+',');
+                }
+            });
+            this.selectedOptions = tickedOptions.slice(0, -1);
+        }
+        if(!this.selectedOptions) {
+            this.selectedOptions = 'Select';
+        }
+    }
+
+    getPicklistOptions(dataFromServer) {
+        if(dataFromServer && this.isMultiSelect) {
+            dataFromServer.forEach(function(datum) {
+                datum.isSelected = false;
+            });
+        }
+        return dataFromServer;
     }
 }
